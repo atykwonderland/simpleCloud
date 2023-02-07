7b11bf35f1d68d3cb6e63f4de3e3be6d7383298e
Alice Kang <43328684+atykwonderland@users.noreply.github.com>
2023-02-06 22:07:46 -0500

    cloud toolset #1-5 done

--- Client/cloud_toolset.py
+++ Client/cloud_toolset.py
@@ -9,6 +9,28 @@ def cloud_hello(url):
     cURL.setopt(cURL.URL, url)
     cURL.perform()
 
+#------------------------ALICE-------------------------
+
+def cloud_init(url):
+    cURL.setopt(cURL.URL, url + '/cloud/init')
+    cURL.perform()
+
+def cloud_pod_register(url, command):
+    command_list = command.split()
+    if len(command_list) == 4:
+        cURL.setopt(cURL.URL, url + '/cloud/pods/' + command_list[3])
+        cURL.perform()
+    else:
+        print('Error: incorrect number of arguments')
+
+def cloud_pod_rm(url, command):
+    command_list = command.split()
+    if len(command_list) == 4:
+        ret = requests.delete(url + '/cloud/pods/' + command_list[3])
+        print(ret.text)
+    else:
+        print('Error: incorrect number of arguments')
+
 def cloud_register(url, command):
     command_list = command.split()
     if len(command_list) == 3:
@@ -17,6 +39,18 @@ def cloud_register(url, command):
     elif len(command_list) == 4:
         cURL.setopt(cURL.URL, url + '/cloud/nodes/' + command_list[2] + '/' + command_list[3])
         cURL.perform()
+    else:
+        print('Error: incorrect number of arguments')
+
+def cloud_rm(url, command):
+    command_list = command.split()
+    if len(command_list) == 3:
+        ret = requests.delete(url + '/cloud/nodes/' + command_list[2])
+        print(ret.text)
+    else:
+        print('Error: incorrect number of arguments')
+
+#------------------------HANA-------------------------
 
 def cloud_launch(url, command):
     command_list = command.split()
@@ -27,6 +61,8 @@ def cloud_launch(url, command):
             ret = requests.post(url + '/cloud/jobs/launch', files=files)
             print(ret.text)
 
+#-------------------------------------------------
+
 #TODO: anything with pass needs a new method to replace it below
 def main():
     rm_url = sys.argv[1]
@@ -35,15 +71,15 @@ def main():
         if command == 'cloud hello':
             cloud_hello(rm_url)
         elif command == 'cloud init':
-            pass
+            cloud_init(rm_url)
         elif command.startswith('cloud pod register'):
-            pass
+            cloud_pod_register(rm_url, command)
         elif command.startswith('cloud pod rm'):
-            pass
+            cloud_pod_rm(rm_url, command)
         elif command.startswith('cloud reigster'):
             cloud_register(rm_url, command)
         elif command.startswith('cloud rm'):
-            pass
+            cloud_rm(rm_url, command)
         elif command.startswith('cloud launch'):
             cloud_launch(rm_url, command)
         elif command.startswith('cloud abort'):
