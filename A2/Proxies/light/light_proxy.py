bb38361947f32cce9684f6d373e9a069e9e1a6bc
Hana <76257528+iitshana@users.noreply.github.com>
2023-03-17 01:05:47 +0800

    Update light_proxy.py

--- A2/Proxies/light/light_proxy.py
+++ A2/Proxies/light/light_proxy.py
@@ -67,7 +67,7 @@ def cloud_init():
                     'id': id,
                     'name': 'light_pod'})
 
-@app.route('/cloud/nodes/<name>/<pod_name>') 
+@app.route('/cloudproxy/node/register/<name>/<pod_name>') 
 def cloud_node(name, pod_name):
     print('Request to register new node: ' + str(name) + ' in pod ' + str(pod_name))
     try:
@@ -100,7 +100,7 @@ def cloud_node(name, pod_name):
         result = str(pod_name) + " not found"
         return jsonify({'result': result, 'node_status': 'not created', 'node_name': str(name)})
 
-@app.route('/cloud/nodes/rm/<name>/<pod_name>')   
+@app.route('/cloudproxy/node/rm/<name>/<pod_name>')   
 def cloud_pod_node_rm(name, pod_name):
     print('Request to remove node: ' + str(name) + 'from pod' + str(pod_name))
     try:
