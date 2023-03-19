e15f1f7be55558c514905a424da3f355513d5fdb
Hana <76257528+iitshana@users.noreply.github.com>
2023-03-20 05:20:23 +0800

    Update app.py

--- A2/Proxies/medium/medium_app/app.py
+++ A2/Proxies/medium/medium_app/app.py
@@ -15,7 +15,7 @@ def medium():
     if len(sys.argv) < 2:
         return 'something went wrong!'
     
-    img = cv.imread('Image.jpg', 0)
+    img = cv.imread('./Image.jpg', 0)
     rows, cols = img.shape
     M = np.float32([[1, 0, 100], [0, 1, 50]])
     dst = cv.warpAffine(img, M, (cols, rows))
