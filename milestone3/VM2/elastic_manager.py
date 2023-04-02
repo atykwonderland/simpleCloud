c166261bb938e7e0ef1dad5c3790b34cd9c094d2
Hana <76257528+iitshana@users.noreply.github.com>
2023-04-02 23:45:08 +0800

    Update elastic_manager.py

--- milestone3/VM2/elastic_manager.py
+++ milestone3/VM2/elastic_manager.py
@@ -24,11 +24,24 @@ def cloud_elasticity_upper():
 
 @app.route('/cloudelastic/elasticity/enable/<pod_name>/<lower>/<upper>')
 def cloud_elasticity_enable(pod_name, lower, upper):
-    
+    if pod_name == "light_pod" or pod_name == "medium_pod" or pod_name == "heavy_pod":
+        pod_limits[pod_name]['upper'] = upper
+        pod_limits[pod_name]['lower'] = lower
+    else:
+        return jsonify({'response': 'failure',
+                        'reason': 'unknown pod'})
+   
 
 @app.route('/cloudelastic/elasticity/disable/<pod_name>')
-def cloud_elasticity_disable():
-    pass
+def cloud_elasticity_disable(pod_name):
+    print('Request to disable elasticity for pod: '  + str(pod_name))
+    for pod in pods:
+        if pod['name'] == pod_name:
+            pod['isElastic'] = flase
+            return jsonify({'response': 'success',
+                    'reason': 'successfully disabled elasticity for pod ' + pod_name})
+    return jsonify({'response': 'failure',
+                        'reason': 'pod not found'})
 
 if __name__ == '__main__':
     app.run(debug=True, host= '0.0.0.0', port=5002)
