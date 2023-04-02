9b45736f0459720736be9091401f08286a1e6221
Alice Kang <43328684+atykwonderland@users.noreply.github.com>
2023-04-02 10:14:27 -0400

    restructure

---
+++ milestone3/VM3/light/app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import sys
+
+app = Flask(__name__)
+
+@app.route('/light/app')
+def light():
+    if len(sys.argv) < 2:
+        return 'something went wrong!'
+    return 'Hello from: ' + sys.argv[1] + '! \n'
+
+if __name__ == '__main__':
+    app.run(debug = True, host='0.0.0.0', port=5000)
