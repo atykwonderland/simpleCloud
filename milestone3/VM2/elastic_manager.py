fad66f388d482f60934c243d38834923afdede53
Alice Kang <43328684+atykwonderland@users.noreply.github.com>
2023-04-02 11:33:22 -0400

    elastic set upper

--- milestone3/VM2/elastic_manager.py
+++ milestone3/VM2/elastic_manager.py
@@ -10,6 +10,12 @@ light_proxy = 'http://10.140.17.109:6001'
 medium_proxy = 'http://10.140.17.109:6002'
 heavy_proxy = 'http://10.140.17.109:6003'
 
+thresholds = {
+    'light_pod': {'upper':None, 'lower':None},
+    'medium_pod': {'upper':None, 'lower':None},
+    'heavy_pod': {'upper':None, 'lower':None}
+}
+
 cURL = pycurl.Curl()
 app = Flask(__name__)
 
@@ -19,8 +25,17 @@ def cloud_elasticity_lower():
     pass
 
 @app.route('/cloudelastic/elasticity/upper/<pod_name>/<value>')
-def cloud_elasticity_upper():
-    pass
+def cloud_elasticity_upper(pod_name, value):
+    # set threshold value for pod
+    if pod_name == "light_pod" or pod_name == "medium_pod" or pod_name == "heavy_pod":
+        thresholds[pod_name]['upper'] = value
+    else:
+        return jsonify({'response': 'failure',
+                        'reason': 'unknown pod'})
+    # trigger thread to refresh?
+
+    return jsonify({'response': 'success',
+                    'result': 'upper threshold set to: ' + str(value)})
 
 @app.route('/cloudelastic/elasticity/enable/<pod_name>/<lower>/<upper>')
 def cloud_elasticity_enable():
